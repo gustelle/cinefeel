63ae72af70a7721771342643a2ea927ffa85cafd
gustelle <guillaume.patin@gmail.com>
2025-11-04 17:58:16 +0100

    feat: increase concurrency

--- backend/start-server.sh
+++ backend/start-server.sh
@@ -25,9 +25,8 @@ prefect gcl create api-rate-limiting --limit 10 --slot-decay-per-second 2.0
 # *********************************
 # heavy tasks limit (e.g., extractors)
 # *********************************
-prefect config set PREFECT_TASK_RUN_TAG_CONCURRENCY_SLOT_WAIT_SECONDS=10
 prefect --no-prompt concurrency-limit delete heavy  # in case it already exists
-prefect concurrency-limit create heavy 5
+prefect concurrency-limit create heavy 10 
 
 
 # *********************************
