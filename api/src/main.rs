ab0311a2d0c0b207dc17ab1b61c7512b55f7a2d2
gustelle <guillaume.patin@gmail.com>
2025-11-15 20:44:27 +0100

    feat: http response

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,11 +6,14 @@ mod interfaces;
 mod entities;
 
 use std::collections::HashMap;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 use crate::{interfaces::{DbRepository}, repositories::{PersonRepository}};
- 
-fn main() {
-    
+
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+
     let repo = PersonRepository::new();
 
     let mut params = HashMap::new();
@@ -18,14 +21,32 @@ fn main() {
 
     let results = repo.query("MATCH (n: Person {title: $title}) RETURN n;", params);
 
+    let mut response = "Results:\n".to_string();
+
     match results {
         Some(records) => {
             for record   in records {
-                println!("{}", record);
-                
+                response.push_str(&format!("{}\n", record));
             }
         },
-        None => println!("No results found or query failed."),
+        None => {
+            response.push_str("No records found.\n");
+        }
     }
+
+    HttpResponse::Ok().body(response)
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+ 
 
